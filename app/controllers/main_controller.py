91b1faf4c6d9557d28bffef43fd34d640ebdfa97
Wade <115143365+Waddenn@users.noreply.github.com>
2023-10-08 21:55:27 +0200

    separating constants in a file

--- app/controllers/main_controller.py
+++ app/controllers/main_controller.py
@@ -3,24 +3,20 @@ import threading
 import subprocess
 import platform
 
+
 class MainController:
     def __init__(self, view=None, model=None):
         self.view = view
         self.model = model
-        self.buttons = []
-        self.movie_links_list = []
-        self.size_var_list = []
-        self.size_options_list = []
-        self.dropdown_menus = []
 
     def on_entry_focus_in(self, event):
-        if self.view.search_entry.get() == 'Search...':
+        if self.view.search_entry.get() == "Search...":
             self.view.search_entry.delete(0, tk.END)
             self.view.search_entry.config(fg="white", font=("Segoe UI", 12, "bold"))
 
     def on_entry_focus_out(self, event):
         if not self.view.search_entry.get():
-            self.view.search_entry.insert(0, 'Search...')
+            self.view.search_entry.insert(0, "Search...")
             self.view.search_entry.config(fg="gray", font=("Segoe UI", 12))
 
     def on_enter(self, event):
@@ -31,17 +27,19 @@ class MainController:
 
     def open_movie(self, index):
         def open_vlc():
-            selected_size = self.size_var_list[index].get()
-            movie_link = self.movie_links_list[index][self.size_options_list[index].index(selected_size)]
+            selected_size = self.view.size_var_list[index].get()
+            movie_link = self.view.movie_links_list[index][
+                self.view.size_options_list[index].index(selected_size)
+            ]
 
             os_name = platform.system()
 
             if os_name == "Windows":
-                vlc_path = "C:/Program Files/VideoLAN/VLC/vlc.exe"  
+                vlc_path = "C:/Program Files/VideoLAN/VLC/vlc.exe"
             elif os_name == "Linux":
-                vlc_path = "/usr/bin/vlc"  
-            elif os_name == "Darwin": 
-                vlc_path = "/Applications/VLC.app/Contents/MacOS/VLC"  
+                vlc_path = "/usr/bin/vlc"
+            elif os_name == "Darwin":
+                vlc_path = "/Applications/VLC.app/Contents/MacOS/VLC"
             else:
                 print("Unsupported operating system.")
                 return
@@ -64,12 +62,17 @@ class MainController:
             movies = self.model.extract_movie_data(html)
             print(f"Results for {movies}:")
             self.view.display_posters(movies)
-            self.view.create_size_dropdowns(movies, self.movie_links_list, self.size_var_list, self.size_options_list, self.dropdown_menus)
+            self.view.create_size_dropdowns(
+                movies,
+                self.view.movie_links_list,
+                self.view.size_var_list,
+                self.view.size_options_list,
+                self.view.dropdown_menus,
+            )
 
         finally:
             self.view.after(0, self._enable_button)
             self.view.stop_gif_animation()
+
     def _enable_button(self):
         self.view.search_button.config(state=tk.NORMAL)
-
-
--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import tkinter as tk
 from app.views.main_view import MainView
 from app.controllers.main_controller import MainController
 from app.models.model import WebDataModel
+from app.constants import (
+    QUALITY_LABELS,
+    SECONDARY_LABELS,
+    EXCLUDE_SOURCES,
+    LOGO_FILEPURSUIT_PATH,
+    LOADING_CUBE_PATH,
+    GLASS_BUTTON_PATH,
+)
 
 
 class App(tk.Tk):
@@ -11,12 +19,20 @@ class App(tk.Tk):
         self.geometry("400x350")
         self.resizable(width=False, height=False)
         self.configure(bg="#04050f")
-        self.model = WebDataModel()
-
+        self.model = WebDataModel(EXCLUDE_SOURCES)
         self.controller = MainController(model=self.model)
-        self.view = MainView(self, self.controller)
+        self.view = MainView(
+            self,
+            self.controller,
+            QUALITY_LABELS,
+            SECONDARY_LABELS,
+            LOGO_FILEPURSUIT_PATH,
+            LOADING_CUBE_PATH,
+            GLASS_BUTTON_PATH,
+        )
         self.view.pack(expand=tk.YES, fill=tk.BOTH)
         self.controller.view = self.view
+
 
 if __name__ == "__main__":
     app = App()
